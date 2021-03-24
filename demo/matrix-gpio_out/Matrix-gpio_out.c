5d29678fc49ca9e5ff50898e4462879812ad97d3
Mert çevik <35631301+MertCEVIK9@users.noreply.github.com>
2021-03-24 22:18:13 +0300

    as
    as

--- demo/matrix-gpio_out/Matrix-gpio_out.c
+++ demo/matrix-gpio_out/Matrix-gpio_out.c
@@ -7,12 +7,16 @@
 
 int main(int argc, char ** argv) 
 {
-    int pin = GPIO_PIN(15); 
+    int pin = GPIO_PIN(7); 
     int i, value, board;
     int ret = -1;
     
-    
-    
+    if ((board = boardInit()) < 0) {
+        printf("Fail to init board\n");
+        return -1;
+    }
+    if (board == BOARD_NANOPC_T2 || board == BOARD_NANOPC_T3)
+        pin = GPIO_PIN(15);
     
     if (argc == 2)
         pin = GPIO_PIN(atoi(argv[1]));

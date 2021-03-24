fe6967804f53daa0f9e71530b2211f825269114d
Mert çevik <35631301+MertCEVIK9@users.noreply.github.com>
2021-03-24 21:44:51 +0300

    Update Matrix-gpio_out.c

--- demo/matrix-gpio_out/Matrix-gpio_out.c
+++ demo/matrix-gpio_out/Matrix-gpio_out.c
@@ -7,28 +7,25 @@
 
 int main(int argc, char ** argv) 
 {
-    int pin ; 
+    int pin = GPIO_PIN(7); 
     int i, value, board;
     int ret = -1;
-	
     
     if ((board = boardInit()) < 0) {
         printf("Fail to init board\n");
         return -1;
     }
-    if (board == BOARD_NANOPC_T2)
+    if (board == BOARD_NANOPC_T2 || board == BOARD_NANOPC_T3)
         pin = GPIO_PIN(15);
     
     if (argc == 2)
         pin = GPIO_PIN(atoi(argv[1]));
     if ((ret = exportGPIOPin(pin)) == -1) {   
         printf("exportGPIOPin(%d) failed\n", pin);
-    
+    }
     if ((ret = setGPIODirection(pin, GPIO_OUT)) == -1) {
         printf("setGPIODirection(%d) failed\n", pin);
-
-	
-	
+    }
     for (i = 0; i < STATUS_CHANGE_TIMES; i++) {
         if (i % 2) {
             value = GPIO_HIGH;
